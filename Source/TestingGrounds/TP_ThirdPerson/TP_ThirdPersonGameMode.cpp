1a51e232740e3acf866bb8e296af259303b68545
Vanny Sou <accusard@me.com>
2019-01-23 11:04:22 -0800

    Rearrange files

--- Source/TestingGrounds/TP_ThirdPerson/TP_ThirdPersonGameMode.cpp
+++ Source/TestingGrounds/TP_ThirdPerson/TP_ThirdPersonGameMode.cpp
@@ -7,7 +7,7 @@
 ATP_ThirdPersonGameMode::ATP_ThirdPersonGameMode()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPerson/Blueprints/ThirdPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/Character/Blueprints/ThirdPersonCharacter.ThirdPersonCharacter'"));
 	if (PlayerPawnBPClass.Class != NULL)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
